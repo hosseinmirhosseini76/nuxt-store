acc9baca050b9d25cd3c55684c5a0583983b0722
Hossein Mirhosseini <hosseinmirhosseini76@gmail.com>
2022-07-11 19:47:32 +0430

    refactor(app/CustomFooter): a tags changed

--- components/app/CustomFooter/index.vue
+++ components/app/CustomFooter/index.vue
@@ -8,43 +8,43 @@
               <img src="/images/white-logo.png" alt="hexashop ecommerce templatemo">
             </div>
             <ul>
-              <li><a href="#">16501 Collins Ave, Sunny Isles Beach, FL 33160, United States</a></li>
-              <li><a href="#">hexashop@company.com</a></li>
-              <li><a href="#">010-020-0340</a></li>
+              <li><a href="/">16501 Collins Ave, Sunny Isles Beach, FL 33160, United States</a></li>
+              <li><a href="/">hexashop@company.com</a></li>
+              <li><a href="/">010-020-0340</a></li>
             </ul>
           </div>
         </div>
         <div class="col-lg-3">
           <h4>Shopping &amp; Categories</h4>
           <ul>
-            <li><a href="#">Men’s Shopping</a></li>
-            <li><a href="#">Women’s Shopping</a></li>
-            <li><a href="#">Kid's Shopping</a></li>
+            <li><nuxt-link :to="({name: 'products', query: { category: 'Men' }})">Men’s Shopping</nuxt-link></li>
+            <li><nuxt-link :to="({name: 'products', query: { category: 'Women' }})">Women’s Shopping</nuxt-link></li>
+            <li><nuxt-link :to="({name: 'products', query: { category: 'Jewelry' }})">Jewelries Shopping</nuxt-link></li>
           </ul>
         </div>
         <div class="col-lg-3">
           <h4>Useful Links</h4>
           <ul>
-            <li><a href="#">Homepage</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Help</a></li>
-            <li><a href="#">Contact Us</a></li>
+            <li><a href="/">Homepage</a></li>
+            <li><a href="/">About Us</a></li>
+            <li><a href="/">Help</a></li>
+            <li><a href="/">Contact Us</a></li>
           </ul>
         </div>
         <div class="col-lg-3">
           <h4>Help &amp; Information</h4>
           <ul>
-            <li><a href="#">Help</a></li>
-            <li><a href="#">FAQ's</a></li>
-            <li><a href="#">Shipping</a></li>
-            <li><a href="#">Tracking ID</a></li>
+            <li><a href="/">Help</a></li>
+            <li><a href="/">FAQ's</a></li>
+            <li><a href="/">Shipping</a></li>
+            <li><a href="/">Tracking ID</a></li>
           </ul>
         </div>
         <div class="col-lg-12">
           <div class="under-footer">
             <p>Copyright © 2022 HexaShop Co., Ltd. All Rights Reserved.
 
-              <br>Design: <a href="https://templatemo.com" target="_parent" title="free css templates">TemplateMo</a></p>
+              <br>Design: TemplateMo & Develop: <a href="https://hossein.dev">Hossein.dev</a></p>
             <ul>
               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
