389232cb14a60dcc0537b6b485da30d80dc11dae
Hossein Mirhosseini <hosseinmirhosseini76@gmail.com>
2022-07-12 18:37:29 +0430

    refactor(products): add to cart func added

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -83,7 +83,9 @@
                         @click="goToProductDetailPage(product)"
                       ><a><i class="fa fa-eye"></i></a></li>
                       <li><a><i class="fa fa-star"></i></a></li>
-                      <li><a><i class="fa fa-shopping-cart"></i></a></li>
+                      <li
+                        @click="$store.dispatch('cart/addToCart', product)"
+                      ><a><i class="fa fa-shopping-cart"></i></a></li>
                     </ul>
                   </div>
                   <img :src="product.image" alt="">
