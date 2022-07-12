378675c528a87392505b6b34b608e9ac890baa8b
Hossein Mirhosseini <hosseinmirhosseini76@gmail.com>
2022-07-12 10:37:30 +0430

    refactor: toast plugin added

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/custom.js'
+    '~/plugins/custom.js',
+    '~/plugins/vueToast.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
